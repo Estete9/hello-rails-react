da65da04fa8bf32837cc5d03e611bcf147b11673
Esteban <cepalacios@outlook.com>
2024-02-12 19:35:02 -0500

    implement api endpoint for random greeting

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'greetings/greeting'
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  namespace :api do
+    namespace :v1 do
+      resources :greetings do
+        collection do
+          get :random_greeting
+        end
+      end
+    end
+  end
+end
